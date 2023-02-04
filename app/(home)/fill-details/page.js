0ad37104a0e5e84d4b3c72ada13cd047bf4fbaee
sidak003 <sidakdhingra100@gmail.com>
2023-02-05 03:55:34 +0530

    beta-error-fix

--- app/(home)/fill-details/page.js
+++ app/(home)/fill-details/page.js
@@ -23,7 +23,7 @@ export default async function FillDetailsPage() {
             <HiPencil className={styles.pencilIcon} />
             <h3 className={styles.headline}>Fill Your Details</h3>
             <div className={styles.wrapper}>
-                <FillDetailsSection points={points}/>
+                <OrderForm points={points}/>
                 {/* <OrderForm points={[{"pointId":1,"pointName":"Clock Tower"},{"pointId":2,"pointName":"Pierre Fauchard"},{"pointId":3,"pointName":"Tech Park"},{"pointId":4,"pointName":"University Building"}]}/> */}
             </div>
         </div>
